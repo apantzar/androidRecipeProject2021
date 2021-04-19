a92b60b6be1e2f70bfdbac8c759c2743d0daa63c
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-04-19 19:41:29 +0300

    Basic HashMe added

---
+++ app/src/main/java/com/pantz/recipepro/HashMe.java
@@ -0,0 +1,39 @@
+package com.pantz.recipepro;
+
+
+/**
+ * this class is responsible to encrypt
+ * passwords
+ *
+ *
+ * Algorithm: shhh be quiet (¬‿¬)
+ */
+
+public class HashMe {
+
+
+    private String passwordToHash;
+
+
+    public HashMe(String passwordToHash) {
+        this.passwordToHash = passwordToHash;
+    }
+
+
+    public String getPasswordToHash() {
+        return passwordToHash;
+    }
+
+
+
+
+    public String theHasher(String passwordToHash){
+
+        return "";
+    }
+
+
+}
+
+
+
