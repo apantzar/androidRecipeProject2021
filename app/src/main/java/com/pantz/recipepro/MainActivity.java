f5d3d6dda4d1240660163b32a977f4b3279585ec
onoma <dimipeft@csd.auth.gr>
2021-04-20 17:34:23 +0300

    Main activity

--- app/src/main/java/com/pantz/recipepro/MainActivity.java
+++ app/src/main/java/com/pantz/recipepro/MainActivity.java
@@ -3,6 +3,7 @@ package com.pantz.recipepro;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -38,34 +39,36 @@ public class MainActivity extends AppCompatActivity {
         signin = (Button) findViewById(R.id.btnsignin);
         DB = new DBHelper(this);
 
-        signup.setOnClickListener(view -> {
-            String user = username.getText().toString();
-            String pass = password.getText().toString();
-            String repass = repassword.getText().toString();
+        signup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
 
-            if (user.equals("") || pass.equals("") || repass.equals(""))
-                Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-            else {
-                if (pass.equals(repass)) {
-                    Boolean checkusers = DB.checkusername(user);
-                    if (checkusers == false) {
-                        Boolean insert = DB.insertData(user, pass);
-                        if (insert == true) {
-                            Toast.makeText(MainActivity.this, "Registered Succesfully", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), ActivityHome.class);
-                            startActivity(intent);
+                if (user.equals("") || pass.equals("") || repass.equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else {
+                    if (pass.equals(repass)) {
+                        Boolean checkusers = DB.checkusername(user);
+                        if (checkusers == false) {
+                            Boolean insert = DB.insertData(user, pass);
+                            if (insert == true) {
+                                Toast.makeText(MainActivity.this, "Registered Succesfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), ActivityHome.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(MainActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
                         } else {
-                            Toast.makeText(MainActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "User already exists!", Toast.LENGTH_SHORT).show();
                         }
                     } else {
-                        Toast.makeText(MainActivity.this, "User already exists!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Password not matching", Toast.LENGTH_SHORT).show();
                     }
-                } else {
-                    Toast.makeText(MainActivity.this, "Password not matching", Toast.LENGTH_SHORT).show();
                 }
+
             }
-
-
         });
 
         signin.setOnClickListener(view -> {
