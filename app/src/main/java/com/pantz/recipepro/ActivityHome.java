314d3da437c1e1ceb8dc1047850f474654a9d81d
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-04-23 19:28:33 +0300

    Nothing

--- app/src/main/java/com/pantz/recipepro/ActivityHome.java
+++ app/src/main/java/com/pantz/recipepro/ActivityHome.java
@@ -2,20 +2,23 @@ package com.pantz.recipepro;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpResponse;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+
 
 public class ActivityHome extends AppCompatActivity {
 
@@ -33,9 +36,12 @@ public class ActivityHome extends AppCompatActivity {
         Button button = (Button)findViewById(R.id.button2);
 
         button.setOnClickListener(v -> {
+
+
             // Instantiate the RequestQueue.
             RequestQueue queue = Volley.newRequestQueue(ActivityHome.this);
-            String url ="https://www.google.com"; //here the request
+            String url ="http://api.yummly.com/v1/api/recipe/recipe-id?_app_id=default-application_5197045&_app_key=26a22ee7a7msh03d13c0b22a034cp1317e8jsn479f9b5ec601";
+            //here the request
 
             // Request a string response from the provided URL.
             StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
