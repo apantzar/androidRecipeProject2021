b47800ead619b82b7cca64dff8532a84cadf5560
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-04-27 17:40:19 +0300

    General Fragment added

--- app/src/main/java/com/pantz/recipepro/generalFragment.java
+++ app/src/main/java/com/pantz/recipepro/generalFragment.java
@@ -1,23 +1,19 @@
 package com.pantz.recipepro;
 
-import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ThirdFragment#newInstance} factory method to
+ * Use the {@link generalFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ThirdFragment extends Fragment {
-
-
-    Button generalBtn ;
+public class generalFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +24,7 @@ public class ThirdFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ThirdFragment() {
+    public generalFragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +34,11 @@ public class ThirdFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ThirdFragment.
+     * @return A new instance of fragment generalFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ThirdFragment newInstance(String param1, String param2) {
-        ThirdFragment fragment = new ThirdFragment();
+    public static generalFragment newInstance(String param1, String param2) {
+        generalFragment fragment = new generalFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -57,34 +53,12 @@ public class ThirdFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
-        View v = inflater.inflate(R.layout.fragment_third, container, false);
-
-
-        generalBtn = v.findViewById(R.id.button3);
-
-        generalBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-               generalFragment generalFragment =  new generalFragment();
-                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-                transaction.replace(R.id.thrirdFragment, generalFragment);
-                transaction.commit();
-
-            }
-        });
-
-        return v;
+        return inflater.inflate(R.layout.fragment_general, container, false);
     }
 }
