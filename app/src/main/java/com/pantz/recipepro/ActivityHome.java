6a0960c9cabe90f8d1b9f9b5080b732c964969ca
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-05-12 12:19:53 +0300

    Raw text in array works

--- app/src/main/java/com/pantz/recipepro/ActivityHome.java
+++ app/src/main/java/com/pantz/recipepro/ActivityHome.java
@@ -118,6 +118,53 @@ public class ActivityHome extends AppCompatActivity {
 
                     try{
                         JSONArray recipesArray = inner.getJSONArray("recipes");
+                        JSONObject innerParser = recipesArray.getJSONObject(0);
+                        JSONArray sectionsAr = innerParser.getJSONArray("sections");
+                        JSONObject innerSections = sectionsAr.getJSONObject(0);
+                        JSONArray compArray = innerSections.getJSONArray("components");
+                        String[] rawTextArray = new String[compArray.length()];
+
+                        for(int j=0; j<compArray.length();j++){
+                             //String compValue = compArray.optString("raw_text");
+                            // System.out.println("The raw text is:  "+compValue);
+
+                                JSONObject innerObj = compArray.getJSONObject(j);
+                                for(Iterator it = innerObj.keys(); it.hasNext(); ) {
+                                    String key = (String)it.next();
+                                    System.out.println(key + "!!:" + innerObj.get(key));
+
+                                    if(key.equals("raw_text")){
+
+
+                                        rawTextArray[j] = (String) innerObj.get(key);
+
+                                    }
+                                }
+
+
+                        }
+
+
+
+                        for(int k=0;k<rawTextArray.length; k++){
+
+                            System.out.println("-----------------------[here]--------------------------------------");
+                            System.out.println("Raw text: "+k+ " => "+rawTextArray[k]);
+
+                        }
+
+
+
+                       // JSONArray section = inner.getJSONArray("sections");
+                      //  JSONArray comp = section.getJSONArray(0);
+
+                        System.out.println("\n=========================================================");
+
+                        System.out.println(inner);
+
+                        System.out.println("\n\n=========================================================");
+
+
 
 
                         /*for(int j=0; j<recipesArray.length(); j++){
@@ -129,37 +176,55 @@ public class ActivityHome extends AppCompatActivity {
 
 
 
-                        JSONObject recipeObj = recipesArray.getJSONObject(0);
+                       // JSONObject recipeObj = recipesArray.getJSONObject(0);
+
                       //  JSONObject tagObj = recipesArray.getJSONObject(tagKey);
 
 
-                        String recipeName = recipeObj.getString("name");
+
+
+
+
+
+                       // String recipeName = recipeObj.optString("name");
+
+
                         //String catName = tagObj.getString("display_name");
 
 
                         //System.out.println("The tag is: "+catName);
-                        System.out.println("This will be the name:  "+recipeName);
+                        //System.out.println("This will be the name:  "+recipeName);
                         counterOfRecipes++;
 
+
+
+
                         System.out.println("Recipes Number: "+counterOfRecipes);
+
+
+
+
+                        //for(int compC=0; compC<comp.length(); compC++){
+
+                            //JSONObject compObj = comp.getJSONObject(1);
+                            //String compValue = compObj.optString("raw_text");
+                           // System.out.println("The raw text is:  "+compValue);
+                       // }
+
+
+
+
+
+
                     }catch (Exception e){
 
+                        e.printStackTrace();
 
                     }
-
-
-
-
-
 
                    // String recipeTitle = inner.getString("")
 
                 }
-
-
-
-
-
 
 
             } catch (IOException | JSONException e) {
