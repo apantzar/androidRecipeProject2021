80fe1c03d6a6f8225df18a6d2662a3f4d184b737
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-05-23 23:11:20 +0300

    Details activity added, xml for details added!

---
+++ app/src/main/java/com/pantz/recipepro/activity_details.java
@@ -0,0 +1,14 @@
+package com.pantz.recipepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
